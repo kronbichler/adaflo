3d777f23d60b266391220cb214deffce17160709
Peter Munch <peterrmuench@gmail.com>
2020-12-08 10:55:01 +0100

    Move template instantiation

--- source/level_set_okz_advance_concentration.cc
+++ source/level_set_okz_advance_concentration.cc
@@ -25,7 +25,6 @@
 #include <deal.II/numerics/vector_tools.h>
 
 #include <adaflo/level_set_okz_advance_concentration.h>
-#include <adaflo/level_set_okz_template_instantations_adv.h>
 
 namespace
 {
@@ -63,6 +62,70 @@ namespace
     return Utilities::MPI::max(max_velocity, get_communicator(dof_handler));
   }
 } // namespace
+
+
+
+#define EXPAND_OPERATIONS(OPERATION)                                          \
+  const unsigned int degree_u =                                               \
+    this->matrix_free.get_dof_handler(parameters.dof_index_vel)               \
+      .get_fe()                                                               \
+      .tensor_degree();                                                       \
+  const unsigned int ls_degree = this->parameters.concentration_subdivisions; \
+                                                                              \
+  AssertThrow(degree_u >= 1 && degree_u <= 5, ExcNotImplemented());           \
+  AssertThrow(ls_degree >= 1 && ls_degree <= 4, ExcNotImplemented());         \
+  if (ls_degree == 1)                                                         \
+    {                                                                         \
+      if (degree_u == 1)                                                      \
+        OPERATION(1, 1);                                                      \
+      else if (degree_u == 2)                                                 \
+        OPERATION(1, 2);                                                      \
+      else if (degree_u == 3)                                                 \
+        OPERATION(1, 3);                                                      \
+      else if (degree_u == 4)                                                 \
+        OPERATION(1, 4);                                                      \
+      else if (degree_u == 5)                                                 \
+        OPERATION(1, 5);                                                      \
+    }                                                                         \
+  else if (ls_degree == 2)                                                    \
+    {                                                                         \
+      if (degree_u == 1)                                                      \
+        OPERATION(2, 1);                                                      \
+      else if (degree_u == 2)                                                 \
+        OPERATION(2, 2);                                                      \
+      else if (degree_u == 3)                                                 \
+        OPERATION(2, 3);                                                      \
+      else if (degree_u == 4)                                                 \
+        OPERATION(2, 4);                                                      \
+      else if (degree_u == 5)                                                 \
+        OPERATION(2, 5);                                                      \
+    }                                                                         \
+  else if (ls_degree == 3)                                                    \
+    {                                                                         \
+      if (degree_u == 1)                                                      \
+        OPERATION(3, 1);                                                      \
+      else if (degree_u == 2)                                                 \
+        OPERATION(3, 2);                                                      \
+      else if (degree_u == 3)                                                 \
+        OPERATION(3, 3);                                                      \
+      else if (degree_u == 4)                                                 \
+        OPERATION(3, 4);                                                      \
+      else if (degree_u == 5)                                                 \
+        OPERATION(3, 5);                                                      \
+    }                                                                         \
+  else if (ls_degree == 4)                                                    \
+    {                                                                         \
+      if (degree_u == 1)                                                      \
+        OPERATION(4, 1);                                                      \
+      else if (degree_u == 2)                                                 \
+        OPERATION(4, 2);                                                      \
+      else if (degree_u == 3)                                                 \
+        OPERATION(4, 3);                                                      \
+      else if (degree_u == 4)                                                 \
+        OPERATION(4, 4);                                                      \
+      else if (degree_u == 5)                                                 \
+        OPERATION(4, 5);                                                      \
+    }
 
 
 
