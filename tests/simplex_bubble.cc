ceaf392c7e1e78509427282adf988909889a4927
Peter Munch <peterrmuench@gmail.com>
2020-12-08 20:53:35 +0100

    Replace file

--- tests/simplex_bubble.cc
+++ tests/simplex_bubble.cc
@@ -1,6 +1,6 @@
 // --------------------------------------------------------------------------
 //
-// Copyright (C) 2020 by the adaflo authors
+// Copyright (C) 2010 - 2016 by the adaflo authors
 //
 // This file is part of the adaflo library.
 //
@@ -13,341 +13,226 @@
 //
 // --------------------------------------------------------------------------
 
-#include <deal.II/base/function.h>
-#include <deal.II/base/function_time.h>
-#include <deal.II/base/index_set.h>
-#include <deal.II/base/logstream.h>
-#include <deal.II/base/parameter_handler.h>
-#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/base/utilities.h>
+// runs a simulation on a static bubble where the velocities ideally should be
+// zero but where we actually get some velocities which are due to
+// inaccuracies in the scheme
 
-#include <deal.II/distributed/grid_refinement.h>
-#include <deal.II/distributed/shared_tria.h>
-
-#include <deal.II/dofs/dof_tools.h>
-
-#include <deal.II/fe/fe_q.h>
+#include <deal.II/distributed/tria.h>
 
 #include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/grid_in.h>
-#include <deal.II/grid/grid_out.h>
-#include <deal.II/grid/manifold_lib.h>
-#include <deal.II/grid/tria.h>
-#include <deal.II/grid/tria_accessor.h>
-#include <deal.II/grid/tria_iterator.h>
 
-#include <deal.II/lac/affine_constraints.h>
-#include <deal.II/lac/la_parallel_vector.h>
+#include <adaflo/level_set_okz.h>
+#include <adaflo/level_set_okz_matrix.h>
+#include <adaflo/parameters.h>
+#include <adaflo/phase_field.h>
 
-#include <deal.II/numerics/data_out.h>
-#include <deal.II/numerics/vector_tools.h>
-
-#include <deal.II/simplex/grid_generator.h>
-
-#include <adaflo/navier_stokes.h>
-#include <adaflo/time_stepping.h>
-
-#include <fstream>
-#include <iomanip>
-#include <iostream>
-
-#define MESH_VERSION 1
 
 using namespace dealii;
 
 
-
-// @sect4{Inflow}
-
-template <int dim>
-class InflowVelocity : public Function<dim>
+struct TwoPhaseParameters : public FlowParameters
 {
-public:
-  InflowVelocity()
-    : Function<dim>(dim)
-  {}
-
-  virtual void
-  vector_value(const Point<dim> &p, Vector<double> &values) const
+  TwoPhaseParameters(const std::string &parameter_filename)
   {
-    AssertDimension(values.size(), dim);
-
-    (void)p;
-
-    values(0) = 1;
-    for (unsigned int d = 1; d < dim; ++d)
-      values(d) = 0;
+    ParameterHandler prm;
+    FlowParameters::declare_parameters(prm);
+    prm.enter_subsection("Problem-specific");
+    prm.declare_entry("two-phase method",
+                      "level set okz",
+                      Patterns::Selection(
+                        "level set okz|level set okz matrix|phase field"),
+                      "Defines the two-phase method to be used");
+    prm.leave_subsection();
+    check_for_file(parameter_filename, prm);
+    parse_parameters(parameter_filename, prm);
+    prm.enter_subsection("Problem-specific");
+    solver_method = prm.get("two-phase method");
+    prm.leave_subsection();
   }
 
-private:
+  std::string solver_method;
 };
 
 
 template <int dim>
-class FlowPastCylinder
+class InitialValuesLS : public Function<dim>
 {
 public:
-  FlowPastCylinder(FlowParameters &parameters);
+  InitialValuesLS()
+    : Function<dim>(1, 0)
+  {}
+
+  double
+  value(const Point<dim> &p, const unsigned int /*component*/) const
+  {
+    const double radius               = 0.25;
+    Point<dim>   distance_from_origin = p;
+    for (unsigned int i = 0; i < dim; ++i)
+      distance_from_origin[i] = 0.5;
+    return p.distance(distance_from_origin) - radius;
+  }
+};
+
+
+
+// @sect3{The <code>MicroFluidicProblem</code> class template}
+template <int dim>
+class MicroFluidicProblem
+{
+public:
+  MicroFluidicProblem(const TwoPhaseParameters &parameters);
   void
   run();
 
 private:
-  void
-  output_results() const;
-
+  MPI_Comm           mpi_communicator;
   ConditionalOStream pcout;
 
-  mutable TimerOutput timer;
+  TwoPhaseParameters                        parameters;
+  parallel::distributed::Triangulation<dim> triangulation;
 
-  parallel::shared::Triangulation<dim> triangulation;
-
-  const bool         use_simplex_mesh;
-  const unsigned int n_refinements;
-
-  NavierStokes<dim> navier_stokes;
+  std::unique_ptr<TwoPhaseBaseAlgorithm<dim>> solver;
 };
 
 
-
-namespace
-{
-  template <int dim>
-  Point<dim>
-  get_direction()
-  {
-    Point<dim> direction;
-    direction[dim - 1] = 1.;
-    return direction;
-  }
-
-  template <int dim>
-  Point<dim>
-  get_center()
-  {
-    Point<dim> center;
-    center[0] = 0.5;
-    center[1] = 0.2;
-    return center;
-  }
-
-  unsigned int
-  fix_n_refinements(FlowParameters &parameters)
-  {
-    unsigned int temp = parameters.global_refinements;
-
-    parameters.global_refinements = 0;
-
-    return temp;
-  }
-
-} // namespace
-
-
-
 template <int dim>
-FlowPastCylinder<dim>::FlowPastCylinder(FlowParameters &parameters)
-  : pcout(std::cout, (Utilities::MPI::this_mpi_process(MPI_COMM_WORLD) == 0))
-  , timer(pcout, TimerOutput::summary, TimerOutput::cpu_and_wall_times)
-  , triangulation(MPI_COMM_WORLD)
-  , use_simplex_mesh(parameters.use_simplex_mesh)
-  , n_refinements(fix_n_refinements(parameters))
-  , navier_stokes(parameters, triangulation, &timer)
-{}
+MicroFluidicProblem<dim>::MicroFluidicProblem(const TwoPhaseParameters &parameters)
+  : mpi_communicator(MPI_COMM_WORLD)
+  , pcout(std::cout, Utilities::MPI::this_mpi_process(mpi_communicator) == 0)
+  ,
 
-
-
-template <int dim>
-void
-FlowPastCylinder<dim>::output_results() const
+  parameters(parameters)
+  , triangulation(mpi_communicator)
 {
-  timer.enter_subsection("Generate output.");
-
-  navier_stokes.output_solution(navier_stokes.get_parameters().output_filename);
-
-  timer.leave_subsection();
-}
-
-
-
-void create_triangulation(Triangulation<2> & tria,
-                          const bool         use_simplex_mesh,
-                          const unsigned int n_refinements)
-{
-  const unsigned int n = Utilities::pow(2, n_refinements);
-
-  if (use_simplex_mesh)
-    {
-      if (false)
-        {
-          GridGenerator::subdivided_hyper_rectangle_with_simplices(
-            tria, {5 * n, 1 * n}, Point<2>(0.0, 0.0), Point<2>(5.0, 1.0), false);
-        }
-      else
-        {
-          GridIn<2> grid_in;
-          grid_in.attach_triangulation(tria);
-          std::ifstream input_file("simplex_channel_" + std::to_string(n_refinements) +
-                                   ".msh");
-          grid_in.read_msh(input_file);
-        }
-
-      for (auto cell : tria)
-        for (auto face : cell.face_iterators())
-          {
-            const auto p = face->center();
-
-            if (std::abs(p[0] - 0.0) < 1e-8)
-              face->set_boundary_id(0);
-            else if (std::abs(p[0] - 5.0) < 1e-8)
-              face->set_boundary_id(1);
-            else if (std::abs(p[1] - 0.0) < 1e-8)
-              face->set_boundary_id(2);
-            else if (std::abs(p[1] - 1.0) < 1e-8)
-              face->set_boundary_id(3);
-          }
-    }
+  if (parameters.solver_method == "level set okz")
+    solver = std::make_unique<LevelSetOKZSolver<dim>>(parameters, triangulation);
+  else if (parameters.solver_method == "level set okz matrix")
+    solver = std::make_unique<LevelSetOKZMatrixSolver<dim>>(parameters, triangulation);
+  else if (parameters.solver_method == "phase field")
+    solver = std::make_unique<PhaseFieldSolver<dim>>(parameters, triangulation);
   else
-    {
-      GridGenerator::subdivided_hyper_rectangle(
-        tria, {5 * n, 1 * n}, Point<2>(0.0, 0.0), Point<2>(5.0, 1.0), true);
-    }
-}
-
-
-
-void create_triangulation(Triangulation<3> & tria,
-                          const bool         use_simplex_mesh,
-                          const unsigned int n_refinements)
-{
-  const unsigned int n = Utilities::pow(2, n_refinements);
-
-  if (use_simplex_mesh)
-    {
-      GridGenerator::subdivided_hyper_rectangle_with_simplices(tria,
-                                                               {5 * n, 1 * n, 1 * n},
-                                                               Point<3>(0.0, 0.0, 0.0),
-                                                               Point<3>(5.0, 1.0, 1.0),
-                                                               true);
-
-      for (auto cell : tria)
-        for (auto face : cell.face_iterators())
-          {
-            const auto p = face->center();
-
-            if (std::abs(p[0] - 0.0) < 1e-8)
-              face->set_boundary_id(0);
-            else if (std::abs(p[0] - 5.0) < 1e-8)
-              face->set_boundary_id(1);
-            else if (std::abs(p[1] - 0.0) < 1e-8)
-              face->set_boundary_id(2);
-            else if (std::abs(p[1] - 1.0) < 1e-8)
-              face->set_boundary_id(3);
-            else if (std::abs(p[2] - 0.0) < 1e-8)
-              face->set_boundary_id(4);
-            else if (std::abs(p[2] - 1.0) < 1e-8)
-              face->set_boundary_id(5);
-          }
-    }
-  else
-    {
-      GridGenerator::subdivided_hyper_rectangle(tria,
-                                                {5 * n, 1 * n, 1 * n},
-                                                Point<3>(0.0, 0.0, 0.0),
-                                                Point<3>(5.0, 1.0, 1.0),
-                                                true);
-    }
+    AssertThrow(false, ExcMessage("Unknown solver selected in parameter file"));
 }
 
 
 
 template <int dim>
 void
-FlowPastCylinder<dim>::run()
+MicroFluidicProblem<dim>::run()
 {
-  timer.enter_subsection("Setup grid and initial condition.");
-  pcout << "Running a " << dim << "D flow past a cylinder "
-        << "using " << navier_stokes.time_stepping.name() << ", Q"
-        << navier_stokes.get_fe_u().degree << "/Q" << navier_stokes.get_fe_p().degree
-        << " elements" << std::endl;
+  // create mesh
+  std::vector<unsigned int> subdivisions(dim, 5);
+  subdivisions[dim - 1] = 10;
 
-  create_triangulation(triangulation, use_simplex_mesh, n_refinements);
+  const Point<dim> bottom_left;
+  const Point<dim> top_right = (dim == 2 ? Point<dim>(1, 2) : Point<dim>(1, 1, 2));
+  GridGenerator::subdivided_hyper_rectangle(triangulation,
+                                            subdivisions,
+                                            bottom_left,
+                                            top_right);
 
-  for (unsigned int i = 2; i < 2 * dim; ++i)
-    navier_stokes.set_no_slip_boundary(i);
+  // set boundary indicator to 2 on left and right face -> symmetry boundary
+  typename parallel::distributed::Triangulation<dim>::active_cell_iterator
+    cell = triangulation.begin(),
+    endc = triangulation.end();
 
-  navier_stokes.set_velocity_dirichlet_boundary(0,
-                                                std::make_shared<InflowVelocity<dim>>());
+  for (; cell != endc; ++cell)
+    for (unsigned int face = 0; face < GeometryInfo<dim>::faces_per_cell; ++face)
+      if (cell->face(face)->at_boundary() &&
+          (std::fabs(cell->face(face)->center()[0] - 1) < 1e-14 ||
+           std::fabs(cell->face(face)->center()[0]) < 1e-14))
+        cell->face(face)->set_boundary_id(2);
 
-  navier_stokes.set_open_boundary(1, std::make_shared<Functions::ZeroFunction<dim>>(1));
-  timer.leave_subsection();
+  AssertThrow(parameters.global_refinements < 12, ExcInternalError());
 
-  navier_stokes.setup_problem(InflowVelocity<dim>());
-  navier_stokes.print_n_dofs();
-  output_results();
+  solver->set_no_slip_boundary(0);
+  solver->fix_pressure_constant(0);
+  solver->set_symmetry_boundary(2);
 
-  // @sect5{Time loop}
-  while (navier_stokes.time_stepping.at_end() == false)
+  solver->setup_problem(Functions::ZeroFunction<dim>(dim), InitialValuesLS<dim>());
+  solver->output_solution(parameters.output_filename);
+
+  std::vector<std::vector<double>> solution_data;
+  solution_data.push_back(solver->compute_bubble_statistics(0));
+
+  // time loop
+  bool first_output = true;
+  while (solver->get_time_stepping().at_end() == false)
     {
-      navier_stokes.advance_time_step();
+      solver->advance_time_step();
 
-      // We check whether we are at a time step where to save the current
-      // solution to a file.
-      if (navier_stokes.time_stepping.at_tick(
-            navier_stokes.get_parameters().output_frequency))
-        output_results();
+      solver->output_solution(parameters.output_filename);
+
+      solver->refine_grid();
+
+      solution_data.push_back(solver->compute_bubble_statistics());
+
+      if (solution_data.size() > 0 &&
+          Utilities::MPI::this_mpi_process(triangulation.get_communicator()) == 0 &&
+          solver->get_time_stepping().at_tick(parameters.output_frequency))
+        {
+          const int time_step = 1.000001e4 * solver->get_time_stepping().step_size();
+
+          std::ostringstream filename3;
+          filename3 << parameters.output_filename << "-"
+                    << Utilities::int_to_string((int)parameters.adaptive_refinements, 1)
+                    << "-" << Utilities::int_to_string(parameters.global_refinements, 3)
+                    << "-" << Utilities::int_to_string(time_step, 4) << ".txt";
+
+          std::fstream output_positions3(filename3.str().c_str(),
+                                         first_output ? std::ios::out :
+                                                        std::ios::out | std::ios::app);
+
+          output_positions3.precision(14);
+          if (first_output)
+            output_positions3
+              << "#    time        area      perimeter   circularity   bubble_xvel   bubble_yvel   bubble_xpos    bubble_ypos"
+              << std::endl;
+          for (unsigned int i = 0; i < solution_data.size(); ++i)
+            {
+              output_positions3 << " ";
+              for (unsigned int j = 0; j < solution_data[i].size(); ++j)
+                output_positions3 << solution_data[i][j] << "   ";
+              output_positions3 << std::endl;
+            }
+          solution_data.clear();
+          first_output = false;
+        }
     }
-
-  if (!navier_stokes.time_stepping.at_tick(
-        navier_stokes.get_parameters().output_frequency))
-    output_results();
 }
-
-
-
-/* ----------------------------------------------------------------------- */
-
-
-
-/* ----------------------------------------------------------------------- */
 
 
 
 int
 main(int argc, char **argv)
 {
+  using namespace dealii;
+
+
   try
     {
-      using namespace dealii;
-
-      Utilities::MPI::MPI_InitFinalize mpi_initialization(argc,
-                                                          argv,
-                                                          numbers::invalid_unsigned_int);
       deallog.depth_console(0);
+      Utilities::MPI::MPI_InitFinalize mpi_init(argc, argv, -1);
 
       std::string paramfile;
       if (argc > 1)
         paramfile = argv[1];
       else
-        paramfile = "flow_past_cylinder.prm";
+        paramfile = "rising_bubble.prm";
 
-      FlowParameters parameters(paramfile);
+      TwoPhaseParameters parameters(paramfile);
       if (parameters.dimension == 2)
         {
-          FlowPastCylinder<2> channel(parameters);
-          channel.run();
+          MicroFluidicProblem<2> flow_problem(parameters);
+          flow_problem.run();
         }
       else if (parameters.dimension == 3)
         {
-          Assert(false, ExcNotImplemented());
-          /*
-          FlowPastCylinder<3> channel(parameters);
-          channel.run();
-           */
+          MicroFluidicProblem<3> flow_problem(parameters);
+          flow_problem.run();
         }
       else
-        {
-          AssertThrow(false, ExcMessage("Invalid dimension"));
-        }
+        AssertThrow(false, ExcNotImplemented());
     }
   catch (std::exception &exc)
     {
