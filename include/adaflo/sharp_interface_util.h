cf657a4fe7b4732b6543efa531ed4e2aabf5fef0
Peter Munch <peterrmuench@gmail.com>
2021-05-08 09:15:28 +0200

    Use dealii::GridTools::MarchingCubeAlgorithm

--- include/adaflo/sharp_interface_util.h
+++ include/adaflo/sharp_interface_util.h
@@ -24,6 +24,7 @@
 #include <deal.II/fe/fe_q_iso_q1.h>
 #include <deal.II/fe/mapping_fe_field.h>
 
+#include <deal.II/grid/grid_tools.h>
 #include <deal.II/grid/grid_tools_cache.h>
 
 #include <deal.II/matrix_free/fe_evaluation.h>
@@ -230,173 +231,6 @@ namespace dealii
       }
     }
   } // namespace GridTools
-
-  namespace GridGenerator
-  {
-    template <int dim>
-    class MarchingCubeAlgorithm
-    {
-    public:
-      MarchingCubeAlgorithm(const Mapping<dim, dim> &      mapping,
-                            const FiniteElement<dim, dim> &fe,
-                            const unsigned int             n_subdivisions)
-        : n_subdivisions(n_subdivisions)
-        , fe_values(mapping,
-                    fe,
-                    create_qudrature_rule(n_subdivisions),
-                    update_values | update_quadrature_points)
-        , ls_values(fe_values.n_quadrature_points)
-      {
-        AssertDimension(dim, 2);
-      }
-
-      template <typename CellType, typename VectorType>
-      void
-      process_cell(const CellType &                  cell,
-                   const VectorType &                ls_vector,
-                   std::vector<Point<dim>> &         vertices,
-                   std::vector<::CellData<dim - 1>> &cells)
-      {
-        fe_values.reinit(cell);
-        fe_values.get_function_values(ls_vector, ls_values);
-
-        for (unsigned int j = 0; j < n_subdivisions; ++j)
-          for (unsigned int i = 0; i < n_subdivisions; ++i)
-            {
-              std::vector<unsigned int> mask{(n_subdivisions + 1) * (j + 0) + (i + 0),
-                                             (n_subdivisions + 1) * (j + 0) + (i + 1),
-                                             (n_subdivisions + 1) * (j + 1) + (i + 1),
-                                             (n_subdivisions + 1) * (j + 1) + (i + 0),
-                                             (n_subdivisions + 1) * (n_subdivisions + 1) +
-                                               (n_subdivisions * j + i)};
-
-              process_sub_cell(
-                ls_values, fe_values.get_quadrature_points(), mask, vertices, cells);
-            }
-      }
-
-    private:
-      static Quadrature<dim>
-      create_qudrature_rule(const unsigned int n_subdivisions)
-      {
-        /** Example: n_subdivisions = 2
-         *
-         *  x,y in [0,1]x[0,1]
-         *
-         *      ^
-         *    y |
-         *
-         *     (6)   (7)    (8)
-         *      +_____+_____+
-         *      | (11) (12) |
-         *      |  * (4) *  |
-         *   (3)+     +     +(5)
-         *      | (9)  (10) |
-         *      |  *     *  |
-         *      +_____+_____+ --> x
-         *      (0)   (1)  (2)
-         */
-        std::vector<Point<dim>> quadrature_points;
-
-        for (unsigned int j = 0; j <= n_subdivisions; ++j)
-          for (unsigned int i = 0; i <= n_subdivisions; ++i)
-            quadrature_points.emplace_back(1.0 / n_subdivisions * i,
-                                           1.0 / n_subdivisions * j);
-
-        for (unsigned int j = 0; j < n_subdivisions; ++j)
-          for (unsigned int i = 0; i < n_subdivisions; ++i)
-            quadrature_points.emplace_back(1.0 / n_subdivisions * (i + 0.5),
-                                           1.0 / n_subdivisions * (j + 0.5));
-
-        return {quadrature_points};
-      }
-
-      static void
-      process_sub_cell(const std::vector<double> &       ls_values,
-                       const std::vector<Point<dim>> &   points,
-                       const std::vector<unsigned int>   mask,
-                       std::vector<Point<dim>> &         vertices,
-                       std::vector<::CellData<dim - 1>> &cells)
-      {
-        // cases 0-15
-        unsigned int c = 0;
-
-        for (unsigned int i = 0, scale = 1; i < 4; ++i, scale *= 2)
-          c += (ls_values[mask[i]] > 0) * scale;
-
-        if (c == 0 || c == 15)
-          return; // nothing to do since the level set function is constant within the
-                  // sub_cell
-
-        const auto process_points = [&](const auto &lines) {
-          const double w0 = std::abs(ls_values[mask[lines[0]]]);
-          const double w1 = std::abs(ls_values[mask[lines[1]]]);
-
-          return points[mask[lines[0]]] * (w1 / (w0 + w1)) +
-                 points[mask[lines[1]]] * (w0 / (w0 + w1));
-        };
-
-        const auto process_lines = [&](const auto &lines) {
-          std::array<std::array<unsigned int, 2>, 4> table{
-            {{{0, 3}}, {{1, 2}}, {{0, 1}}, {{3, 2}}}};
-
-          const auto p0 = process_points(table[lines[0]]);
-          const auto p1 = process_points(table[lines[1]]);
-
-          cells.resize(cells.size() + 1);
-          cells.back().vertices[0] = vertices.size();
-          cells.back().vertices[1] = vertices.size() + 1;
-
-          vertices.emplace_back(p0);
-          vertices.emplace_back(p1);
-        };
-
-        // Check if the isoline for level set values larger than zero is the element's
-        // diagonal and level set values on both sides from the diagonal are smaller than
-        // zero. In this case, the level set would be a "hat"-function which does not make
-        // sense.
-        if (c == 5 || c == 10)
-          {
-            // cases with two contour lines in cell
-            Assert(false, ExcNotImplemented());
-            return;
-          }
-
-        static const unsigned int X = -1;
-        // numbers are edges of square
-        /*   ___3___
-            |       |
-            0       1
-            |___2___|
-        */
-        std::array<std::array<unsigned int, 2>, 16> table{{
-          {{X, X}},
-          {{0, 2}},
-          {{1, 2}},
-          {{0, 1}}, //  c=0-3
-          {{1, 3}},
-          {{X, X}},
-          {{2, 3}},
-          {{0, 3}}, //  c=4-7
-          {{0, 3}},
-          {{2, 3}},
-          {{X, X}},
-          {{1, 3}}, //  c=8-11
-          {{0, 1}},
-          {{2, 1}},
-          {{0, 2}},
-          {{X, X}} //   c=12-15
-        }};
-
-        process_lines(table[c]);
-      }
-
-      const unsigned int  n_subdivisions;
-      FEValues<dim>       fe_values;
-      std::vector<double> ls_values;
-    };
-  } // namespace GridGenerator
-
 } // namespace dealii
 
 
@@ -979,10 +813,10 @@ compute_force_vector_sharp_interface(const Quadrature<dim - 1> &surface_quad,
 
 
 {
-  const unsigned int                        n_subdivisions = 3;
-  GridGenerator::MarchingCubeAlgorithm<dim> mc(mapping,
-                                               dof_handler.get_fe(),
-                                               n_subdivisions);
+  const unsigned int                                n_subdivisions = 3;
+  GridTools::MarchingCubeAlgorithm<dim, VectorType> mc(mapping,
+                                                       dof_handler.get_fe(),
+                                                       n_subdivisions);
 
   AffineConstraints<double> constraints; // TODO: use the right ones
 
@@ -1015,7 +849,7 @@ compute_force_vector_sharp_interface(const Quadrature<dim - 1> &surface_quad,
         std::vector<::CellData<dim - 1>> surface_cells;
 
         // run square/cube marching algorithm
-        mc.process_cell(cell, ls_vector, surface_vertices, surface_cells);
+        mc.process_cell(cell, ls_vector, 0.0, surface_vertices, surface_cells);
 
         if (surface_vertices.size() == 0)
           return {}; // cell is not cut by interface -> no quadrature points have the be
